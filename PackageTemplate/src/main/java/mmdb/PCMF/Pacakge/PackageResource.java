380b3713803ef091829eb305d0bd9335bb305139
Jax <liugs963@gmail.com>
2016-07-21 00:39:47 +0800

    fix the bug of param transfer

--- PackageTemplate/src/main/java/mmdb/PCMF/Pacakge/PackageResource.java
+++ PackageTemplate/src/main/java/mmdb/PCMF/Pacakge/PackageResource.java
@@ -4,23 +4,27 @@ import java.io.*;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 
+import com.sun.jersey.multipart.FormDataParam;
+
 @Path("/services")
 public class PackageResource {
 	
     @GET 
     @Path("getIt")
     @Produces("text/html")
-    public Response getIt(@QueryParam("get") String get) throws IOException {
+    public Response getIt(@QueryParam("get") String get) throws IOException, InterruptedException {
     	
+    	Thread.sleep(10000);
     	return Response.status(200).entity("I got " + get).build();
     }
     
     @POST 
     @Path("getItPost")
-    @Produces("text/html")
-    public Response getItPost(@QueryParam("get") String get) throws IOException {
+    @Produces("application/x-www-form-urlencoded")
+    public Response getItPost(@FormParam("get") String get,
+    						  @FormParam("get2") String get2) throws IOException {
     	
-    	return Response.status(200).entity("I got " + get).build();
+    	return Response.status(200).entity("I got " + get + "and got + " + get2).build();
     }    
        
 }
