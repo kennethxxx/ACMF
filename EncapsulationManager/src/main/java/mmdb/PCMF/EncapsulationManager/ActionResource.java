72fdfc454695544f5059e6a90829a48a6842f4b5
Jax <jax.ncku@gmail.com>
2016-08-18 14:15:51 +0800

    finished EncapuslationManger

--- EncapsulationManager/src/main/java/mmdb/PCMF/EncapsulationManager/ActionResource.java
+++ EncapsulationManager/src/main/java/mmdb/PCMF/EncapsulationManager/ActionResource.java
@@ -9,14 +9,17 @@ import com.google.gson.Gson;
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
 
-import java.math.BigInteger;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Properties;
+
+import org.apache.oozie.client.OozieClient;
+import org.apache.oozie.client.OozieClientException;
+import org.apache.oozie.client.WorkflowJob;
 
 import org.apache.commons.io.IOUtils;
 import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
@@ -27,21 +30,26 @@ import org.apache.commons.io.FileUtils;
 public class ActionResource {
 
 	private String _baseDir = "/tmp/file-accept/";
-//	DBC dbc = new DBC();
-	
-	
+	DBC dbc = new DBC();
+
     @GET 
     @Path("getIt")
     public Response getIt() {
     	
-    	return Response.ok("aaa").build();
+    	return Response.ok("aaa")
+    			.header("Access-Control-Allow-Origin", "*")
+    			.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT")
+    			.build();
     }
     
     @GET 
     @Path("retrieve/{fileName}")
     public Response retrieve(@PathParam("fileName") String fileName) throws IOException { 
         File mergedPath = new File(this._baseDir, fileName);
-        return Response.ok(mergedPath).build();
+        return Response.ok(mergedPath)
+    			.header("Access-Control-Allow-Origin", "*")
+    			.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT")
+    			.build();
     }
     
     @DELETE
@@ -49,11 +57,27 @@ public class ActionResource {
     public Response delete(@PathParam("fileName") String fileName) throws IOException {
         File mergedPath = new File(this._baseDir, fileName);
         FileUtils.deleteQuietly(mergedPath);
-        return Response.ok("OK").build();
+        return Response.ok("OK")
+    			.header("Access-Control-Allow-Origin", "*")
+    			.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT")
+        		.build();
     }
 
 
-    @PUT
+    @GET
+    @Path("getAction")
+    public Response getAction() throws IOException {
+    	
+    	return Response.ok(dbc.getAction())
+    			.header("Access-Control-Allow-Origin", "*")
+    			.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
+    			.header("Access-Control-Allow-Credentials", "true")
+    			.header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
+    			.build();
+    	
+    }
+    
+    @POST
     @Path("insert")
     @Consumes(MediaType.MULTIPART_FORM_DATA) 
     public Response insert(@FormDataParam("action_info") String action_info,
@@ -62,41 +86,46 @@ public class ActionResource {
         
         OutputStream os = null;
         try {
-            
+            System.out.println("get the request: " + action_info);
         	if(checkWAR(fileDetail.getFileName())){
         		
 	        	fileDetail.getType();
-	        	String fileName = fileDetail.getFileName() + new Date().getTime();
-	        	String tmpName = md5(fileName);
-	            File mergedPath = new File(this._baseDir, tmpName);
+	        	String fileName = fileDetail.getFileName();
+	            File mergedPath = new File(this._baseDir, fileName);
 	            os = new FileOutputStream(mergedPath);
 	            IOUtils.copy(is, os);
 	            
 				Type mapType = new TypeToken<HashMap<String, String>>(){}.getType();  
 				HashMap<String, String> input_para = new Gson().fromJson(action_info, mapType);
-	     //       dbc.newAction(input_para);
-	    //        ArrayList<String> hosts = dbc.getWorkerHost(input_para.get("action_info"));
-	        //    for(String host : hosts ){
+	            dbc.newAction(input_para);
+	            ArrayList<String> hosts = dbc.getWorkerHost(input_para.get("action_owner"));
+	            for(String host : hosts ){
 	            	
-	       //     	copyFileToWorker("http://" + host + ":8181/operator/deploy/"+fileDetail.getFileName(), mergedPath);
-	       //     }
+	            	copyFileToWorker("http://" + host + ":8181/operator/deploy/"+fileDetail.getFileName(), mergedPath);
+	            }
 	            
 	            
         	}else {
         		
-        		return Response.serverError().entity("file type is wrong").build();
+        		return Response.serverError().entity("file type is wrong")
+            			.header("Access-Control-Allow-Origin", "*")
+            			.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
+            			.header("Access-Control-Allow-Credentials", "true")
+            			.header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")            			
+        				.build();
         		
         	}
         
         } catch (IOException ex) {
             
             ex.printStackTrace();
-            Response.status(500).build();
-        } catch (NoSuchAlgorithmException e){
-        	
-        	e.printStackTrace();
-        	Response.status(500).build();
-        	
+    		return Response.serverError()
+        			.header("Access-Control-Allow-Origin", "*")
+        			.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
+        			.header("Access-Control-Allow-Credentials", "true")
+        			.header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")            			
+    				.build();
+            
         } finally {
             
             if (is != null){
@@ -117,23 +146,84 @@ public class ActionResource {
             
         }
         
-        return Response.ok("OK").build();
+        return Response.ok("OK")
+    			.header("Access-Control-Allow-Origin", "*")
+    			.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
+    			.header("Access-Control-Allow-Credentials", "true")
+    			.header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")    			
+        		.build();
         
     }
-  
-    private String md5(String text) throws NoSuchAlgorithmException {
-    	
-    	MessageDigest md = MessageDigest.getInstance("MD5");
-    	byte[] messageDiget = md.digest(text.getBytes());
-    	BigInteger number = new BigInteger(1, messageDiget);
-    	String tmpName = number.toString(16);
 
-        while (tmpName.length() < 32) {
-        	tmpName = "0" + tmpName;
-        }
-        
-        return tmpName;
-  	
+    @GET
+    @Path("getProcess")
+    public Response getProcess() throws IOException {
+    	
+    	return Response.ok(dbc.getProcessLog())
+    			.header("Access-Control-Allow-Origin", "*")
+    			.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
+    			.header("Access-Control-Allow-Credentials", "true")
+    			.header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
+    			.build();
+    	
+    }    
+    
+    @POST
+    @Path("processSubmit")
+    @Consumes(MediaType.MULTIPART_FORM_DATA) 
+    public Response processSubmit(
+    		@FormDataParam("file") InputStream is,
+    		@FormDataParam("file") FormDataContentDisposition fileDetail ) throws IOException, OozieClientException, InterruptedException{
+    	
+    		Date now = new Date();
+    		SimpleDateFormat ft = new SimpleDateFormat("yyyyMMddhhmmss");
+    		String wfNum = ft.format(now);
+    		
+        	String fileName = fileDetail.getFileName();
+            File mergedPath = new File(this._baseDir, fileName);
+            FileOutputStream os = new FileOutputStream(mergedPath);
+            IOUtils.copy(is, os);
+            
+            String process_id = dbc.newProcess();
+            
+            Process p;
+            p = Runtime.getRuntime().exec("hadoop fs -cp /user/root/jobTemplate /user/root/" + wfNum);
+            p.waitFor();
+            p = Runtime.getRuntime().exec("hadoop fs -put " + mergedPath + " /user/root/"+ wfNum);
+            p.waitFor();
+    	
+    		OozieClient wc = new OozieClient("http://node1:11000/oozie");
+    		Properties conf = wc.createConfiguration();
+    		conf.setProperty(OozieClient.APP_PATH, "hdfs://node1:8020/user/root/"+ wfNum);
+    		
+    		conf.setProperty("jobTracker", "node1:8021");
+    		conf.setProperty("nameNode", "hdfs://node1:8020");
+    		conf.setProperty("queueName", "default");
+    		conf.setProperty("processId", process_id);
+    		String jobId = wc.run(conf);
+    		dbc.updateProcessBusyStatus(process_id);
+    		System.out.println("Workflow job submitted");
+    		
+    	    // wait until the workflow job finishes printing the status every 10 seconds
+    	    while (wc.getJobInfo(jobId).getStatus() == WorkflowJob.Status.RUNNING) {
+    	        System.out.println("Workflow job running ...");
+    	        Thread.sleep(10 * 1000);
+    	    }
+    	
+    	    // print the final status of the workflow job
+    	    System.out.println("Workflow job completed ...");
+    	    System.out.println(wc.getJobInfo(jobId));
+    	    String result = dbc.getProcessResultFromActionLogger(process_id);
+    	    dbc.updateProcessFinishedStatusAndSetResult(process_id, result);
+    		
+    	     return Response.ok("OK")
+    	    			.header("Access-Control-Allow-Origin", "*")
+    	    			.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
+    	    			.header("Access-Control-Allow-Credentials", "true")
+    	    			.header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")    			
+    	        		.build();
+    	
+    
     }
     
     private boolean checkWAR(String name) {
--- EncapsulationManager/src/main/java/mmdb/PCMF/EncapsulationManager/DBC.java
+++ EncapsulationManager/src/main/java/mmdb/PCMF/EncapsulationManager/DBC.java
@@ -10,18 +10,12 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import java.util.Properties;
-import org.apache.log4j.PropertyConfigurator;
-import org.apache.log4j.Logger;
 
-import com.google.gson.Gson;
-import com.google.common.reflect.TypeToken;
+import com.google.gson.JsonObject;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.lang.reflect.Type;
-
-import org.json.JSONObject; 
 
 public class DBC {
 	
@@ -80,14 +74,33 @@ public class DBC {
 	  private String getProperties(String key) {
 		  return props.getProperty(key);
 	  }
-	  
+
+	  public String getAction() {
+		  
+		  JsonObject result = null;
+		  try {
+			  
+			  stat = con.createStatement();
+			  String sql = "SELECT * FROM action_table";
+			  System.out.println("SQL: " + sql);
+			  rs = stat.executeQuery(sql);
+			  result = ResultSetToJson.ResultSetToJsonObject(rs);
+			  
+		  }catch(SQLException e) {
+			  
+			  e.getStackTrace();
+			  
+		  }
+		  
+		  return result.toString();		  
+	  }  	  
 	  
 	  public void newAction(HashMap<String, String> action_info){
 		  
 		  try{	  
 		  
 			  // create a new action in table
-			  String sql = "INSERT INTO action_table(action_owner, action_name, action_input, action_output, action_method, contetn_type, prefix) "
+			  String sql = "INSERT INTO action_table(action_owner, action_name, action_input, action_output, action_method, content_type, prefix) "
 			  		+ "VALUES (?,?,?,?,?,?,?)";
 			  pst = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
 			  pst.setString(1, action_info.get("action_owner"));
@@ -113,14 +126,146 @@ public class DBC {
 		  	 
 	  }
 	  
+	  public void updateProcessBusyStatus(String process_id) {
+		  
+		  String sql = "UPDATE process_logger SET `status` = ? " +
+				  							"WHERE `process_id` = ?";
+		  try {
+			  pst = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+			  pst.setInt(1, 1);
+			  pst.setString(2, process_id);
+			  System.out.println(pst.toString());
+			  pst.executeUpdate();
+			  System.out.println("Record is updated status to process_logger table");
+
+		  }catch (SQLException e) {
+			  
+			  e.printStackTrace();
+			  
+		  }
+
+	  }	  
+	  
+	  public void updateProcessFinishedStatusAndSetResult(String process_id, String result) {
+		  
+		  String sql = "UPDATE process_logger SET `status` = ?, `result` = ? " +
+				  							"WHERE `process_id` = ?";
+		  try {
+			  pst = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+			  pst.setInt(1, 2);
+			  pst.setString(2, result);
+			  pst.setString(3, process_id);
+			  System.out.println(pst.toString());
+			  pst.executeUpdate();
+			  System.out.println("Record is updated status to process_logger table");
+
+		  }catch (SQLException e) {
+			  
+			  e.printStackTrace();
+			  
+		  }
+
+	  }
+	  
+	  public String getProcessLog() {
+		  
+		  JsonObject result = null;
+		  try {
+			  
+			  stat = con.createStatement();
+			  String sql = "SELECT * FROM process_logger";
+			  System.out.println("SQL: " + sql);
+			  rs = stat.executeQuery(sql);
+			  result = ResultSetToJson.ResultSetToJsonObject(rs);
+			  
+		  }catch(SQLException e) {
+			  
+			  e.getStackTrace();
+			  
+		  }
+		  
+		  return result.toString();		  
+	  }
+	  
+	  public String getProcessResultFromActionLogger(String process_id) {
+		  
+		  String result = null;
+		  
+		  try {
+			  stat = con.createStatement();
+			  String sql = "SELECT task_result FROM action_logger WHERE process_id = '" + process_id + "'";
+			  System.out.println("SQL: " + sql);
+			  rs = stat.executeQuery(sql);
+			  while( rs.next() ) {
+				  result = rs.getString("task_result");
+			  }
+			  
+		  }catch(SQLException e) {
+			  
+			  e.getStackTrace();
+			  
+		  }
+		  
+		  return result;
+		  	
+	  }
+
+	  public String newProcess(){
+		  
+		  try{	  
+		  
+			  // create a new action in table
+			  String sql = "INSERT INTO process_logger(status) "
+			  		+ "VALUES (0)";
+			  long id = 0;
+			  
+			  pst = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+			  System.out.println("SQL: " + pst.toString());
+			  int affectedRow = pst.executeUpdate();
+			  
+			  if(affectedRow == 0 ){
+				  
+				  throw new SQLException("Creating fail, no rows affected");
+				  
+			  }
+			  
+
+			  ResultSet generatedKeys = pst.getGeneratedKeys();
+			  if (generatedKeys.next()) {
+				  id = generatedKeys.getLong(1);
+			  }
+			  else {
+				  throw new SQLException("Creating user failed, no ID obtained.");
+			  }
+
+		      return String.valueOf(id);
+		  
+		  }catch(Exception e){
+			  
+			  e.printStackTrace();
+			 
+		  }finally{
+			  
+			  
+			  Close();
+			  
+		  }
+		  
+		  return null;
+		  	 
+	  }	  
+
+	  
+	  
 	  public ArrayList<String> getWorkerHost(String owner) {
 		  
 		  String id = null;
 		  ArrayList<String> hosts = new ArrayList<String>();
 		  
 		  try {
-			  
+			  stat = con.createStatement();
 			  String sql = "SELECT worker_host FROM worker_table WHERE worker_type = '" + owner + "'";
+			  System.out.println("SQL: " + sql);
 			  rs = stat.executeQuery(sql);
 			  while( rs.next() ) {
 				  hosts.add(rs.getString("worker_host"));
